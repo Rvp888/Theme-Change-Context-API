e9ecbc35034c4567967463e4553865b2346e0c57
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-16 09:56:29 +0530

    modify file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,14 +11,14 @@ export default function Header() {
 
   return (
     <div className={theme === "light" ? "light-header" : "dark-header"}>
-      <h1>Image Gallery</h1>
+      <h1>Theme Changer</h1>
       <button><Link to="/">Main</Link></button>
       <button><Link to="/main2">Main-2</Link></button>
       <button onClick={changeTheme}>Change Theme</button>
       <input
         className="search-bar"
         type="search"
-        placeholder="Search an image..."
+        placeholder="Search..."
       />
     </div>
   );
